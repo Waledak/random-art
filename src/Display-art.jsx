a8853f3f1f1fb26dc5e2d2f9e863bab76c0e3ee8
Waledak <tanguy.cirillo@gmail.com>
2020-04-29 21:39:21 +0200

    add some css check in the table

--- src/Display-art.jsx
+++ src/Display-art.jsx
@@ -11,25 +11,33 @@ const DisplayArt = ({ objectToDisplay }) => {
         <table>
           <thead>
             <tr>
-              <th colSpan="2">About the work of art</th>
+              <th colSpan="2">Object Details</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>title:</td>
-              <td>{objectToDisplay.title}</td>
+              <td>{objectToDisplay.title===""?"Unknown data":objectToDisplay.title}</td>
             </tr>
             <tr>
-              <td>Period:</td>
-              <td>{objectToDisplay.period}</td>
+              <td>Date:</td>
+              <td>{objectToDisplay.objectDate===""?"Unknown data":objectToDisplay.objectDate}</td>
+            </tr>
+            <tr>
+              <td>Culture: </td>
+              <td>{objectToDisplay.culture===""?"Unknown data":objectToDisplay.culture}</td>
             </tr>
             <tr>
               <td>Size: </td>
-              <td>{objectToDisplay.dimensions}</td>
+              <td>{objectToDisplay.dimensions===""?"Unknown data":objectToDisplay.dimensions}</td>
             </tr>
             <tr>
-              <td>Style: </td>
-              <td>{objectToDisplay.department}</td>
+              <td>Classification: </td>
+              <td>{objectToDisplay.classification===""?"Unknown data":objectToDisplay.classification}</td>
+            </tr>
+            <tr>
+              <td>Period: </td>
+              <td>{objectToDisplay.period===""?"Unknown data":objectToDisplay.period}</td>
             </tr>
           </tbody>
         </table>
@@ -42,17 +50,25 @@ const DisplayArt = ({ objectToDisplay }) => {
           <tbody>
             <tr>
               <td>Name:</td>
-              <td> {objectToDisplay.artistDisplayName}</td>
+              <td> {objectToDisplay.artistDisplayName===""?"Unknown data":objectToDisplay.artistDisplayName}</td>
             </tr>
             <tr>
               <td>Nationality:</td>
-              <td>{objectToDisplay.artistNationality}</td>
+              <td>{objectToDisplay.artistNationality===""?"Unknown data":objectToDisplay.artistNationality}</td>
             </tr>
             <tr>
               <td>Date (Birth-Death): </td>
               <td>
-                {objectToDisplay.artistBeginDate} -{" "}
-                {objectToDisplay.artistEndDate}
+                {
+                  objectToDisplay.artistBeginDate!=="" && objectToDisplay.artistEndDate!==""
+                  ?objectToDisplay.artistBeginDate+" - "+objectToDisplay.artistEndDate
+                  :objectToDisplay.artistBeginDate!==""
+                  ?objectToDisplay.artistBeginDate
+                  :objectToDisplay.artistEndDate!==""
+                  ?objectToDisplay.artistEndDate
+                  :"Unknown data"
+
+                }
               </td>
             </tr>
           </tbody>
