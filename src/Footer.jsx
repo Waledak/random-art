88f72f48d68894a60cd2bae7f56a5acd4d888701
Waledak <tanguy.cirillo@gmail.com>
2020-04-30 08:08:05 +0200

    begin of something (footer)

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Footer = () =>{
+  return(
+    <footer>
+      <div>
+        <img src="./Met-Museum-Logo.svg" style={{color: "white"}} alt=""/>
+      </div>
+      <div>
+
+      </div>
+    </footer>
+  )
+}
